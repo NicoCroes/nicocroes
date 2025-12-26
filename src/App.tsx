e4cb3974f00a8f80f82d473e62776c08cd753330
astrosuka <yegresvoselok@gmail.com>
2025-12-26 16:38:43 -0300

    Adjust general app styling

--- src/App.tsx
+++ src/App.tsx
@@ -22,26 +22,32 @@ function App() {
     data?.detail?.[language] ?? data?.detail?.es ?? data?.detail?.en ?? null;
 
   return (
-    <>
-      <header className="flex items-start justify-between">
-        <div className="flex gap-4">
-          <h1 className="font-bold">{data?.name && data.name}</h1>
-          {detail && <h2>{detail}</h2>}
-          <NavMenu />
-        </div>
-        <LaguageToggle />
-      </header>
+    <div className="text-rey pb-16">
+      <div className="fixed inset-0 -z-10 h-screen bg-linear-to-b from-[white] to-[#C0C0C0]" />
 
-      <AnimatePresence>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Works />} />
-          <Route path="/dp/:slug" element={<DpWorkPage />} />
-          <Route path="/color" element={<Color />} />
-          <Route path="/color/:slug" element={<ColorWorkPage />} />
-          <Route path="/bio" element={<Bio />} />
-        </Routes>
-      </AnimatePresence>
-    </>
+      <header className="sticky top-0 z-10 flex items-center justify-between bg-linear-to-b from-white/60 to-[#C0C0C000] px-4 pt-4 pb-8 text-xl sm:px-8 sm:py-8">
+        <div>
+          <h1 className="text-3xl uppercase">{data?.name && data.name}</h1>
+          {detail && <h2 className="font-thin">{detail}</h2>}
+        </div>
+        <div className="justify-self-end">
+          <LaguageToggle />
+        </div>
+      </header>
+      <NavMenu />
+
+      <div className="px-4">
+        <AnimatePresence>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Works />} />
+            <Route path="/dp/:slug" element={<DpWorkPage />} />
+            <Route path="/color" element={<Color />} />
+            <Route path="/color/:slug" element={<ColorWorkPage />} />
+            <Route path="/bio" element={<Bio />} />
+          </Routes>
+        </AnimatePresence>
+      </div>
+    </div>
   );
 }
 
