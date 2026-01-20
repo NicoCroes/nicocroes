77efddc08e918c1f804062c459ea90511d1570f3
astrosuka <yegresvoselok@gmail.com>
2026-01-20 11:09:22 -0300

    Delete detail text and clean up styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,24 @@ import Color from "./pages/Color";
 import Bio from "./pages/Bio";
 import LaguageToggle from "./components/LanguageToggle";
 import NavMenu from "./components/NavMenu";
-import useLanguage from "./hooks/useLanguage";
 import Footer from "./components/Footer";
 
 function App() {
   const { data, isLoading, error } = useGeneralData();
   const location = useLocation();
-  const { language } = useLanguage();
 
   if (isLoading) return <div>...</div>;
   if (error) return <div>{error.message}</div>;
 
-  const detail =
-    data?.detail?.[language] ?? data?.detail?.es ?? data?.detail?.en ?? null;
-
   return (
-    <div className="text-rey pb-16">
-      <div className="fixed inset-0 -z-10 h-screen bg-linear-to-b from-[white] to-[#C0C0C0]" />
+    <div className="text-rey">
+      <div className="to-silver fixed inset-0 -z-10 h-screen bg-linear-to-b from-[white]" />
 
-      <header className="sticky top-0 z-10 flex items-center justify-between bg-linear-to-b from-white/60 to-[#C0C0C000] px-4 pt-4 pb-8 text-xl sm:px-8 sm:py-8">
+      <header className="to-white/00 sticky top-0 z-10 flex h-18 items-start justify-between bg-linear-to-b from-white/60 px-4 py-2 text-xl">
         <div>
-          <h1 className="text-3xl uppercase">{data?.name && data.name}</h1>
-          {detail && <h2 className="font-thin">{detail}</h2>}
+          <h1 className="text-xl font-light uppercase">
+            {data?.name && data.name}
+          </h1>
         </div>
         <div className="justify-self-end">
           <LaguageToggle />
@@ -37,17 +33,15 @@ function App() {
       </header>
       <NavMenu />
 
-      <div className="px-4">
-        <AnimatePresence>
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Works />} />
-            <Route path="/dp/:slug" element={<DpWorkPage />} />
-            <Route path="/color" element={<Color />} />
-            <Route path="/color/:slug" element={<ColorWorkPage />} />
-            <Route path="/bio" element={<Bio />} />
-          </Routes>
-        </AnimatePresence>
-      </div>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Works />} />
+          <Route path="/dp/:slug" element={<DpWorkPage />} />
+          <Route path="/color" element={<Color />} />
+          <Route path="/color/:slug" element={<ColorWorkPage />} />
+          <Route path="/bio" element={<Bio />} />
+        </Routes>
+      </AnimatePresence>
       <Footer />
     </div>
   );
