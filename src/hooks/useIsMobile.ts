62d3d0e3337deee8c01c79b0ea941c9bee7fbf6f
astrosuka <yegresvoselok@gmail.com>
2026-02-03 10:17:09 -0300

    Change breakpoint to 765

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useIsMobile = (breakpoint = 640) => {
+const useIsMobile = (breakpoint = 765) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
 
   useEffect(() => {
