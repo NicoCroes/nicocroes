15939762df34acfd18ff9ee4bca1440269b71c5d
astrosuka <yegresvoselok@gmail.com>
2026-02-18 18:04:04 -0300

    Config sanity cdn

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: "r3wsr7it",
   dataset: "production",
-  useCdn: false,
+  useCdn: true,
   apiVersion: "2025-12-02",
 });
